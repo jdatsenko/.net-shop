d82a496b315924992f2eecbdfbadda3741f5d9a9
Dmytro Dzhuha <yu.datsenko@gmail.com>
2024-08-17 18:12:05 +0200

    models added

---
+++ Models/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace CarShop.Models
+{
+    public class Car
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? LongDescription { get; set; }
+        public string? Image { get; set; }
+        public ushort Price { get; set; }
+        public bool IsFavorite { get; set; }
+        public bool Available { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category? Category { get; set; }
+
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace CarShop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? CategoryName { get; set; }
+        public string? Description { get; set; }
+        public List<Car>? Cars { get; set; }
+    }
+}
