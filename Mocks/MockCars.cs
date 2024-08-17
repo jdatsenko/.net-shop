9687c53efba13fb83032dba42fa69dc19efd1c8e
Dmytro Dzhuha <yu.datsenko@gmail.com>
2024-08-18 01:14:08 +0200

    imgs and styles added

--- Mocks/MockCars.cs
+++ Mocks/MockCars.cs
@@ -16,8 +16,8 @@ namespace CarShop.Mocks
                 {
                     new Car { Name = "Tesla Model S", ShortDescription = "Fast car", LongDescription = "Beautiful, fast and very quiet car from Tesla", Image = "/img/tesla.jpg", Price = 45000, IsFavorite = true, Available = true, Category = _categoryCars.AllCategories.First() },
                     new Car { Name = "Ford Fiesta", ShortDescription = "Quiet and peaceful", LongDescription = "A comfortable car for city driving", Image = "/img/fiesta.jpg", Price = 11000, IsFavorite = false, Available = true, Category = _categoryCars.AllCategories.Last() },
-                    new Car { Name = "BMW M3", ShortDescription = "Bold and stylish", LongDescription = "A stylish and comfortable car for city driving", Image = "/img/m3.jpg", Price = 65000, IsFavorite = true, Available = true, Category = _categoryCars.AllCategories.Last() },
-                    new Car { Name = "Mercedes C class", ShortDescription = "Luxury and comfort", LongDescription = "A comfortable car for city driving", Image = "/img/mercedes.jpg", Price = 40000, IsFavorite = false, Available = false, Category = _categoryCars.AllCategories.Last() },
+                    new Car { Name = "BMW M3", ShortDescription = "Bold and stylish", LongDescription = "A stylish and comfortable car for city driving", Image = "/img/bmv.jpg", Price = 65000, IsFavorite = true, Available = true, Category = _categoryCars.AllCategories.Last() },
+                    new Car { Name = "Mercedes C class", ShortDescription = "Luxury and comfort", LongDescription = "A comfortable car for city driving", Image = "/img/merc.jpg", Price = 40000, IsFavorite = false, Available = false, Category = _categoryCars.AllCategories.Last() },
                     new Car { Name = "Nissan Leaf", ShortDescription = "Economic and quiet", LongDescription = "A comfortable car for city driving", Image = "/img/nissan.jpg", Price = 14000, IsFavorite = true, Available = true, Category = _categoryCars.AllCategories.First() }
                 };
             }
